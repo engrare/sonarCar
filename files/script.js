064bd8a4b2735764af860181c06d6e154b783ed1
engrare <147000117+engrare@users.noreply.github.com>
2024-11-14 16:04:14 +0100

    Update script.js

--- files/script.js
+++ files/script.js
@@ -112,7 +112,7 @@ function buttonStateChanged(key, bool_btn_state) {
 				if(key_pressed[i]){
 					break;
 				} else if(i == 0) {
-					moveRobot('s'); //stop
+					moveRobot('p'); //stop
 				}
 			}
 		}
